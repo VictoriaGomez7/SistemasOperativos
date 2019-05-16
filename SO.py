55a55722c867e5c997ed0d898d3f8c689dc44473
VictoriaGomez7 <49539981+VictoriaGomez7@users.noreply.github.com>
2019-05-16 12:00:56 -0500

    Create SO.py

---
+++ SO.py
@@ -0,0 +1,7 @@
+class SistemasOperativos:
+    def PedirNumeros(self):
+        num1=int(input('Ingrese primer numero: '))
+        num2 = int(input('Ingrese segundo numero: '))
+
+S=SistemasOperativos()
+S.PedirNumeros()
