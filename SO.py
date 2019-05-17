76c63c9423496d2163dfbc7f6c0b68bde5ff1ffb
AndrewY97 <49506511+AndrewY97@users.noreply.github.com>
2019-05-17 00:08:49 -0500

    Update SO.py

--- SO.py
+++ SO.py
@@ -1,7 +1,14 @@
 class SistemasOperativos:
     def PedirNumeros(self):
-        num1=int(input('Ingrese primer numero: '))
-        num2 = int(input('Ingrese segundo numero: '))
+        self.num1=int(input('Ingrese primer numero: '))
+        self.num2 = int(input('Ingrese segundo numero: '))
+       
+    def Suma(self):
+        suma=self.num1+self.num2
+        print "La suma es:",suma
+        
+        
 
 S=SistemasOperativos()
 S.PedirNumeros()
+S.Suma()
